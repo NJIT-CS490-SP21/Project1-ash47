3a2559dc5fc03b53cf28966b525baa01a9c2c9ef
EC2 Default User <ec2-user@ip-172-31-50-150.ec2.internal>
2021-02-05 00:03:02 +0000

    webpage dynamically changes, swaps between 3 artists

--- spotify_app.py
+++ spotify_app.py
@@ -9,60 +9,72 @@ app = Flask(__name__)
 
 load_dotenv(find_dotenv())
 
-tocken_url = "https://accounts.spotify.com/api/token"
+def get_tocken(sptfy_id, sptfy_sectret):
+    tocken_url = "https://accounts.spotify.com/api/token"
+    
+    creds = f"{sptfy_id}:{sptfy_secret}"
+    
+    client_creds = base64.b64encode(creds.encode())
+    
+    method = "POST"
+    
+    tocken_data = {
+        "grant_type": "client_credentials"
+    }
+    
+    tocken_header = {
+        "Authorization": f"Basic {client_creds.decode()}"    
+    }
+    
+    r = requests.post(tocken_url, data=tocken_data, headers=tocken_header)
+    tocken_responce = r.json()
+    
+    access_token = tocken_responce['access_token']
+    
+    return access_token
+    
+def get_top_tracks(artist_id, access_token):
+    url = f"https://api.spotify.com/v1/artists/{artist_id}/top-tracks" + "?" + "market=US"
+    
+    method = "POST"
+    
+    request_header = {
+        "Authorization": f"Bearer {access_token}"  
+    }
+    
+    response = requests.get(url, headers=request_header)
+    results = response.json();
+    
+    song_info = []
+    
+    count = 0
+    
+    for track in results['tracks']:
+        song_info.append([[]])
+        for i in range (len(track['artists'])):
+            song_info[count][0].append(track['artists'][i]['name'])
+        song_info[count].append(track['name'])
+        song_info[count].append(track['preview_url'])
+        song_info[count].append(track['album']['images'][0]['url'])
+        count += 1
+    
+    return song_info
+    
 
-creds = f"{os.getenv('sptfy_id')}:{os.getenv('sptfy_sectret')}"
-
-client_creds = base64.b64encode(creds.encode())
-
-method = "POST"
-
-tocken_data = {
-    "grant_type": "client_credentials"
-}
-
-tocken_header = {
-    "Authorization": f"Basic {client_creds.decode()}"    
-}
-
-r = requests.post(tocken_url, data=tocken_data, headers=tocken_header)
-tocken_responce = r.json()
-
-access_token = tocken_responce['access_token']
+sptfy_id = os.getenv('sptfy_id')
+sptfy_secret = os.getenv('sptfy_sectret')
+access_token = get_tocken(sptfy_id, sptfy_secret)
 
 artist = ['4YRxDV8wJFPHPTeXepOstw', 
           '4Ai0pGz6GhQavjzaRhPTvz', 
           '1dVygo6tRFXC8CSWURQJq2']
 
-artist_uri = random.choice(artist)
-
-url = f"https://api.spotify.com/v1/artists/{artist_uri}/top-tracks" + "?" + "market=US"
-
-method = "POST"
-
-request_header = {
-    "Authorization": f"Bearer {access_token}"  
-}
-
-response = requests.get(url, headers=request_header)
-results = response.json();
-
-song_info = []
-
-count = 0
-
-for track in results['tracks']:
-    song_info.append([[]])
-    for i in range (len(track['artists'])):
-        song_info[count][0].append(track['artists'][i]['name'])
-    song_info[count].append(track['name'])
-    song_info[count].append(track['preview_url'])
-    song_info[count].append(track['album']['images'][0]['url'])
-    count += 1
 
 @app.route('/')
 
 def hello_world():
+    artist_id = random.choice(artist)
+    song_info = get_top_tracks(artist_id, access_token)
     return render_template(
         "index.html", 
         len = len(song_info), 
