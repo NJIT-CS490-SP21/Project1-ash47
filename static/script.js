d9c0443800333c8ee0befe8779ad984272c1afee
EC2 Default User <ec2-user@ip-172-31-50-150.ec2.internal>
2021-02-13 05:09:06 +0000

    script check if a div element is missing

--- static/script.js
+++ static/script.js
@@ -64,137 +64,175 @@ function hideLoader()
 
 window.onload = () => { 
     
-    document.getElementById('tabf0').addEventListener('click', () => {
-        console.log('Button was clicked!');
-        
-        var song_name = document.getElementById("name0").innerHTML;
-        var artist_name = document.getElementById("artist_name").innerHTML;
-        var song_image = document.getElementById("image0").src;
-        var song_audio = document.getElementById("audio0").src;
-        var artist_list = document.getElementById("name_list0").innerHTML;
-        
-        showLoader();
-        sendReq(song_name, artist_name, song_image, song_audio, artist_list);
-        
-    });
+    let tab = document.getElementById('tabf0');
+    if(typeof(tab) != 'undefined' && tab != null)
+    {
+        tab.addEventListener('click', () => {
+            console.log('Button was clicked!');
+            
+            var song_name = document.getElementById("name0").innerHTML;
+            var artist_name = document.getElementById("artist_name").innerHTML;
+            var song_image = document.getElementById("image0").src;
+            var song_audio = document.getElementById("audio0").src;
+            var artist_list = document.getElementById("name_list0").innerHTML;
+            showLoader();
+            sendReq(song_name, artist_name, song_image, song_audio, artist_list);
+            
+        });
+    }
     
-    document.getElementById('tabf1').addEventListener('click', () => {
-        console.log('Button was clicked!');
+    tab = document.getElementById('tabf1');
+    if(typeof(tab) != 'undefined' && tab != null)
+    {
+        tab.addEventListener('click', () => {
+            console.log('Button was clicked!');
+            
+            var song_name = document.getElementById("name1").innerHTML;
+            var artist_name = document.getElementById("artist_name").innerHTML;
+            var song_image = document.getElementById("image1").src;
+            var song_audio = document.getElementById("audio1").src;
+            var artist_list = document.getElementById("name_list1").innerHTML;
+            showLoader();
+            sendReq(song_name, artist_name, song_image, song_audio, artist_list);
+            
+        });
+    }
+    
+    tab = document.getElementById('tabf2');
+    if(typeof(tab) != 'undefined' && tab != null)
+    {
+        tab.addEventListener('click', () => {
+            console.log('Button was clicked!');
+            
+            var song_name = document.getElementById("name2").innerHTML;
+            var artist_name = document.getElementById("artist_name").innerHTML;
+            var song_image = document.getElementById("image2").src;
+            var song_audio = document.getElementById("audio2").src;
+            var artist_list = document.getElementById("name_list2").innerHTML;
+            showLoader();
+            sendReq(song_name, artist_name, song_image, song_audio, artist_list);
+            
+        });
+    }
+    
+    tab = document.getElementById('tabf3');
+    if(typeof(tab) != 'undefined' && tab != null)
+    {
+        tab.addEventListener('click', () => {
+            console.log('Button was clicked!');
+            
+            var song_name = document.getElementById("name3").innerHTML;
+            var artist_name = document.getElementById("artist_name").innerHTML;
+            var song_image = document.getElementById("image3").src;
+            var song_audio = document.getElementById("audio3").src;
+            var artist_list = document.getElementById("name_list3").innerHTML;
+            showLoader();
+            sendReq(song_name, artist_name, song_image, song_audio, artist_list);
+            
+        });
+    }
+    
+    tab = document.getElementById('tabf4');
+    if(typeof(tab) != 'undefined' && tab != null)
+    {
+        tab.addEventListener('click', () => {
+            console.log('Button was clicked!');
+            
+            var song_name = document.getElementById("name4").innerHTML;
+            var artist_name = document.getElementById("artist_name").innerHTML;
+            var song_image = document.getElementById("image4").src;
+            var song_audio = document.getElementById("audio4").src;
+            var artist_list = document.getElementById("name_list4").innerHTML;
+            showLoader();
+            sendReq(song_name, artist_name, song_image, song_audio, artist_list);
+            
+        });
+    }
+    tab = document.getElementById('tabf5');
+    if(typeof(tab) != 'undefined' && tab != null)
+    {
+        tab.addEventListener('click', () => {
+            console.log('Button was clicked!');
+            
+            var song_name = document.getElementById("name5").innerHTML;
+            var artist_name = document.getElementById("artist_name").innerHTML;
+            var song_image = document.getElementById("image5").src;
+            var song_audio = document.getElementById("audio5").src;
+            var artist_list = document.getElementById("name_list5").innerHTML;
+            showLoader();
+            sendReq(song_name, artist_name, song_image, song_audio, artist_list);
         
-        var song_name = document.getElementById("name1").innerHTML;
-        var artist_name = document.getElementById("artist_name").innerHTML;
-        var song_image = document.getElementById("image1").src;
-        var song_audio = document.getElementById("audio1").src;
-        var artist_list = document.getElementById("name_list1").innerHTML;
+        });
+    }
+    
+    tab = document.getElementById('tabf6');
+    if(typeof(tab) != 'undefined' && tab != null)
+    {
+        tab.addEventListener('click', () => {
+            console.log('Button was clicked!');
+            
+            var song_name = document.getElementById("name6").innerHTML;
+            var artist_name = document.getElementById("artist_name").innerHTML;
+            var song_image = document.getElementById("image6").src;
+            var song_audio = document.getElementById("audio6").src;
+            var artist_list = document.getElementById("name_list6").innerHTML;
+            showLoader();
+            sendReq(song_name, artist_name, song_image, song_audio, artist_list);
         
-        showLoader();
-        sendReq(song_name, artist_name, song_image, song_audio, artist_list);
-        
-    });
-    document.getElementById('tabf2').addEventListener('click', () => {
-        console.log('Button was clicked!');
-        
-        var song_name = document.getElementById("name2").innerHTML;
-        var artist_name = document.getElementById("artist_name").innerHTML;
-        var song_image = document.getElementById("image2").src;
-        var song_audio = document.getElementById("audio2").src;
-        var artist_list = document.getElementById("name_list2").innerHTML;
-        
-        showLoader();
-        sendReq(song_name, artist_name, song_image, song_audio, artist_list);
-        
-    });
-    document.getElementById('tabf3').addEventListener('click', () => {
-        console.log('Button was clicked!');
-        
-        var song_name = document.getElementById("name3").innerHTML;
-        var artist_name = document.getElementById("artist_name").innerHTML;
-        var song_image = document.getElementById("image3").src;
-        var song_audio = document.getElementById("audio3").src;
-        var artist_list = document.getElementById("name_list3").innerHTML;
-        
-        showLoader();
-        sendReq(song_name, artist_name, song_image, song_audio, artist_list);
-        
-    });
-    document.getElementById('tabf4').addEventListener('click', () => {
-        console.log('Button was clicked!');
-        
-        var song_name = document.getElementById("name4").innerHTML;
-        var artist_name = document.getElementById("artist_name").innerHTML;
-        var song_image = document.getElementById("image4").src;
-        var song_audio = document.getElementById("audio4").src;
-        var artist_list = document.getElementById("name_list4").innerHTML;
-        
-        showLoader();
-        sendReq(song_name, artist_name, song_image, song_audio, artist_list);
-        
-    });
-    document.getElementById('tabf5').addEventListener('click', () => {
-        console.log('Button was clicked!');
-        
-        var song_name = document.getElementById("name5").innerHTML;
-        var artist_name = document.getElementById("artist_name").innerHTML;
-        var song_image = document.getElementById("image5").src;
-        var song_audio = document.getElementById("audio5").src;
-        var artist_list = document.getElementById("name_list5").innerHTML;
-        
-        showLoader();
-        sendReq(song_name, artist_name, song_image, song_audio, artist_list);
-        
-    });
-    document.getElementById('tabf6').addEventListener('click', () => {
-        console.log('Button was clicked!');
-        
-        var song_name = document.getElementById("name6").innerHTML;
-        var artist_name = document.getElementById("artist_name").innerHTML;
-        var song_image = document.getElementById("image6").src;
-        var song_audio = document.getElementById("audio6").src;
-        var artist_list = document.getElementById("name_list6").innerHTML;
-        
-        showLoader();
-        sendReq(song_name, artist_name, song_image, song_audio, artist_list);
-        
-    });
-    document.getElementById('tabf7').addEventListener('click', () => {
-        console.log('Button was clicked!');
-        
-        var song_name = document.getElementById("name7").innerHTML;
-        var artist_name = document.getElementById("artist_name").innerHTML;
-        var song_image = document.getElementById("image7").src;
-        var song_audio = document.getElementById("audio7").src;
-        var artist_list = document.getElementById("name_list7").innerHTML;
-        
-        showLoader();
-        sendReq(song_name, artist_name, song_image, song_audio, artist_list);
-        
-    });
-    document.getElementById('tabf8').addEventListener('click', () => {
-        console.log('Button was clicked!');
-        
-        var song_name = document.getElementById("name8").innerHTML;
-        var artist_name = document.getElementById("artist_name").innerHTML;
-        var song_image = document.getElementById("image8").src;
-        var song_audio = document.getElementById("audio8").src;
-        var artist_list = document.getElementById("name_list8").innerHTML;
-        
-        showLoader();
-        sendReq(song_name, artist_name, song_image, song_audio, artist_list);
-        
-    });
-    document.getElementById('tabf9').addEventListener('click', () => {
-        console.log('Button was clicked!');
-        
-        var song_name = document.getElementById("name9").innerHTML;
-        var artist_name = document.getElementById("artist_name").innerHTML;
-        var song_image = document.getElementById("image9").src;
-        var song_audio = document.getElementById("audio9").src;
-        var artist_list = document.getElementById("name_list9").innerHTML;
-        
-        showLoader();
-        sendReq(song_name, artist_name, song_image, song_audio, artist_list);
-        
-    });
+        });
+    }
+    
+    tab = document.getElementById('tabf7');
+    if(typeof(tab) != 'undefined' && tab != null)
+    {
+        tab.addEventListener('click', () => {
+            console.log('Button was clicked!');
+            
+            var song_name = document.getElementById("name7").innerHTML;
+            var artist_name = document.getElementById("artist_name").innerHTML;
+            var song_image = document.getElementById("image7").src;
+            var song_audio = document.getElementById("audio7").src;
+            var artist_list = document.getElementById("name_list7").innerHTML;
+            showLoader();
+            sendReq(song_name, artist_name, song_image, song_audio, artist_list);
+            
+        });
+    }
+    
+    tab = document.getElementById('tabf8');
+    if(typeof(tab) != 'undefined' && tab != null)
+    {
+        tab.addEventListener('click', () => {
+            console.log('Button was clicked!');
+            
+            var song_name = document.getElementById("name8").innerHTML;
+            var artist_name = document.getElementById("artist_name").innerHTML;
+            var song_image = document.getElementById("image8").src;
+            var song_audio = document.getElementById("audio8").src;
+            var artist_list = document.getElementById("name_list8").innerHTML;
+            showLoader();
+            sendReq(song_name, artist_name, song_image, song_audio, artist_list);
+            
+        });
+    }
+    
+    
+    tab = document.getElementById('tabf9');
+    if(typeof(tab) != 'undefined' && tab != null)
+    {
+        tab.addEventListener('click', () => {
+            console.log('Button was clicked!');
+            
+            var song_name = document.getElementById("name9").innerHTML;
+            var artist_name = document.getElementById("artist_name").innerHTML;
+            var song_image = document.getElementById("image9").src;
+            var song_audio = document.getElementById("audio9").src;
+            var artist_list = document.getElementById("name_list9").innerHTML;
+            showLoader();
+            sendReq(song_name, artist_name, song_image, song_audio, artist_list);
+            
+        });
+    }
     
     var closebtns = document.getElementById("close");
 
